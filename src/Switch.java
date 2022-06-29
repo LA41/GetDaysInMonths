ee27b4f958db4c4aacdcf1efdac0bd5279e4b2ba
LA41 <lu.adamczyk97@gmail.com>
2022-06-29 14:18:13 +0200

    Initial commit

---
+++ src/Switch.java
@@ -0,0 +1,27 @@
+public class Switch {
+    public static void main(String[] args) {
+        System.out.println(isLeapYear(2000));
+        System.out.println(getDaysInMonth(2, 1212));
+    }
+    public static boolean isLeapYear(int year) {
+        if (year < 1 || year > 9999)
+            return false;
+        else
+            return ((year % 400) == 0 || ((year % 4) == 0 && (year % 100) != 0));
+    }
+    public static int getDaysInMonth(int month, int year) {
+        if (month < 1 || month > 12 || year < 1 || year > 9999)
+            return -1;
+        switch (month) {
+            case 4: case 6: case 9: case 11:
+                return 30;
+            case 2:
+                if (isLeapYear(year))
+                    return 29;
+                return 28;
+            default:
+                return 31;
+
+        }
+    }
+}
